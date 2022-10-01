9c7acf32f67f77e130f7a815d28b4063e6e20550
Ameer Taweel <ameertaweel2002@gmail.com>
2022-10-01 21:27:26 +0300

    refactor: remove legacy code

--- legacy/types/phase.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-class Phase(enum.Enum):
-    IDLE = 0
-    FIRST = 1
-    SECOND = 2
--- legacy/util/time.py
+++
@@ -1,5 +0,0 @@
-from time import time as now
-
-def remaining_until(time: float):
-    """Returns the time remaining in seconds, until a given second."""
-    return max(time - now(), 0)
