227f76e89a20c279508603fb0e435da387690993
Ameer Taweel <ameertaweel2002@gmail.com>
2023-02-01 19:08:39 +0300

    chore: install requests library and simplify flake

--- flake.nix
+++ flake.nix
@@ -10,39 +10,15 @@
 	flake-utils.lib.eachDefaultSystem (system: let
 		pkgs = import nixpkgs { inherit system; };
 
-		python = pkgs.python3;
-		pythonPkgs = python.pkgs;
-		buildPythonApplication = pythonPkgs.buildPythonApplication;
-
-		dependencies = rec {
-			python = with pythonPkgs; {
-				build = [ flit ];
-				test = [ pytest pytest-asyncio ];
-				runtime = [ aiohttp phe pyaes rich ];
-			};
-			other = with pkgs; {
-				build = [ ];
-				test = [ ];
-				runtime = [ ];
-			};
-			build = other.build python.build;
-			test = other.test python.test;
-			runtime = other.runtime python.runtime;
-		};
-
-		pythonDevelopmentEnvironment = python.withPackages (_:
-			dependencies.python.build ++
-			dependencies.python.test ++
-			dependencies.python.runtime
-		);
+		pythonEnv = pkgs.python3.withPackages (pythonPkgs: with pythonPkgs; [
+			phe
+			pyaes
+			requests
+			rich 
+		]);
 	in {
 		devShell = pkgs.mkShell {
-			nativeBuildInputs = with pkgs;
-				[ pythonDevelopmentEnvironment ] ++
-				dependencies.other.build ++
-				dependencies.other.test ++
-				dependencies.other.runtime
-			;
+			nativeBuildInputs = [ pythonEnv ];
 			buildInputs = [ ];
 		};
 	});
