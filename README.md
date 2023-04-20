cc1c902008cde293e18373cb7f2c94ffdc30a2ec
Ameer Taweel <ameertaweel2002@gmail.com>
2023-04-20 09:30:41 +0300

    doc: add instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Proven Smart Grid Security (AggFT)
+
+To run the failure check code:
+
+```bash
+mkdir results
+ruby failure_check.rb >> failure_check.csv
+```
--- failure_check.rb
+++ failure_check.rb
@@ -1,4 +1,4 @@
-puts "PRIVACY TYPE,SM COUNT, FAILURES"
+puts "PRIVACY TYPE,SM COUNT,FAILURES"
 puts `python3 aggft/failure_check.py --privacy-type mask --n 2 --n-min 2`
 puts `python3 aggft/failure_check.py --privacy-type encr --n 2 --n-min 2`
 puts `python3 aggft/failure_check.py --privacy-type mask --n 3 --n-min 2`
