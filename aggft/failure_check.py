8afd1a040ce16957e7707bc18b5089f1c4912236
Ameer Taweel <ameertaweel2002@gmail.com>
2023-04-25 21:49:27 +0300

    feat: update the failure check script to use the new simulator

--- aggft/failure_check.py
+++ aggft/failure_check.py
@@ -45,47 +45,47 @@ print("PRIVACY TYPE,SM COUNT,FAILURES")
 
 for N in CONSTANTS["N_VALUES"]:
     for PRIVACY_TYPE in CONSTANTS["PRIVACY_TYPES"]:
-        args = {**CONSTANTS, "SM_COUNT": N, "PRIVACY_TYPE": PRIVACY_TYPE}
-
-        # Generate Keys
-        prf_keys = tuple([random.randbytes(args["PRF_KEY_LEN"]) for _ in range(N)])
-        pk, sk = paillier.generate_paillier_keypair(
-            n_length=CONSTANTS["PAILLIER_KEY_LEN"]
-        )
-
-        args = {**args, "prf_keys": prf_keys, "pk": pk, "sk": sk}
-
-        registry = None if CONSTANTS["USE_HTTP"] else shared_mem.make_registry(N)
-
-        # Helper functions
-        base_dc_meta = (
-            base_meta.base_dc_masking_meta(args)
-            if PRIVACY_TYPE == "mask"
-            else base_meta.base_dc_paillier_meta(args)
-        )
-        base_sm_meta = (
-            base_meta.base_sm_masking_meta(args)
-            if PRIVACY_TYPE == "mask"
-            else base_meta.base_sm_paillier_meta(args)
-        )
-
-        def make_net_mngr():
-            if CONSTANTS["USE_HTTP"]:
-                return network.HTTPNetworkManager()
-            return network.SharedMemoryNetworkManager(registry)
-
-        reports = simulator.simulate(
-            N,
-            args["STARTUP_WAIT"],
-            link_utils.all_links_configurations(N),
-            make_net_mngr,
-            base_dc_meta,
-            base_sm_meta,
-            args["STARTING_PORT"],
-        )
-
         failures = 0
-        for dc_report, sm_reports in reports:
+        for config in link_utils.all_links_configurations(N):
+            args = {**CONSTANTS, "SM_COUNT": N, "PRIVACY_TYPE": PRIVACY_TYPE}
+
+            # Generate Keys
+            prf_keys = tuple([random.randbytes(args["PRF_KEY_LEN"]) for _ in range(N)])
+            pk, sk = paillier.generate_paillier_keypair(
+                n_length=CONSTANTS["PAILLIER_KEY_LEN"]
+            )
+
+            args = {**args, "prf_keys": prf_keys, "pk": pk, "sk": sk}
+
+            registry = None if CONSTANTS["USE_HTTP"] else shared_mem.make_registry(N)
+
+            # Helper functions
+            base_dc_meta = (
+                base_meta.base_dc_masking_meta(args)
+                if PRIVACY_TYPE == "mask"
+                else base_meta.base_dc_paillier_meta(args)
+            )
+            base_sm_meta = (
+                base_meta.base_sm_masking_meta(args)
+                if PRIVACY_TYPE == "mask"
+                else base_meta.base_sm_paillier_meta(args)
+            )
+
+            def make_net_mngr():
+                if CONSTANTS["USE_HTTP"]:
+                    return network.HTTPNetworkManager()
+                return network.SharedMemoryNetworkManager(registry)
+
+            dc_report, sm_reports = simulator.simulate(
+                N,
+                args["STARTUP_WAIT"],
+                config,
+                make_net_mngr,
+                base_dc_meta,
+                base_sm_meta,
+                args["STARTING_PORT"],
+            )
+
             failures += int(not dc_report.terminated)
 
         # CSV row
