a47db79bb50414a785a836b3aef98fe638099e38
Ameer Taweel <ameertaweel2002@gmail.com>
2023-04-25 21:43:23 +0300

    feat: simulator runs for one config

--- aggft/simulate/simulator.py
+++ aggft/simulate/simulator.py
@@ -11,56 +11,57 @@ from simulate import factories
 def simulate(
     sm_count,
     startup_wait,
-    configurations,
+    configuration,
     make_net_mngr,
     base_dc_meta,
     base_sm_meta,
     starting_port,
 ):
-    for c in configurations:
-        test_start = now()
+    c = configuration
 
-        dc = factories.dc_factory(
+    test_start = now()
+
+    dc = factories.dc_factory(
+        sm_count,
+        test_start,
+        startup_wait,
+        base_dc_meta(),
+        make_net_mngr(),
+        c,
+        starting_port,
+    )
+    dc_thread = threading.Thread(target=dc.run_once)
+
+    sms = ()
+    sm_threads = ()
+    for id in range(sm_count):
+        sm = factories.sm_factory(
+            id,
             sm_count,
             test_start,
             startup_wait,
-            base_dc_meta(),
+            base_sm_meta(id),
             make_net_mngr(),
             c,
             starting_port,
         )
-        dc_thread = threading.Thread(target=dc.run_once)
+        sm_thread = threading.Thread(target=sm.run_once)
+        sm_threads = (*sm_threads, sm_thread)
+        sms = (*sms, sm)
 
-        sms = ()
-        sm_threads = ()
-        for id in range(sm_count):
-            sm = factories.sm_factory(
-                id,
-                sm_count,
-                test_start,
-                startup_wait,
-                base_sm_meta(id),
-                make_net_mngr(),
-                c,
-                starting_port,
-            )
-            sm_thread = threading.Thread(target=sm.run_once)
-            sm_threads = (*sm_threads, sm_thread)
-            sms = (*sms, sm)
+    # Start all threads
+    dc_thread.start()
+    for thread in sm_threads:
+        thread.start()
 
-        # Start all threads
-        dc_thread.start()
-        for thread in sm_threads:
-            thread.start()
+    # Wait for DC thread
+    dc_thread.join()
+    for sm in sms:
+        sm.killed = True
+    for thread in sm_threads:
+        thread.join()
 
-        # Wait for DC thread
-        dc_thread.join()
-        for sm in sms:
-            sm.killed = True
-        for thread in sm_threads:
-            thread.join()
+    dc_report = dc.reports[0]
+    sm_reports = tuple([sm.reports[0] for sm in sms])
 
-        dc_report = dc.reports[0]
-        sm_reports = tuple([sm.reports[0] for sm in sms])
-
-        yield dc_report, sm_reports
+    return dc_report, sm_reports
