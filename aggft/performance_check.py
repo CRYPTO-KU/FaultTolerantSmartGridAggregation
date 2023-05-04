7aff8d7378827154e24b6938e2419b34829b15a6
Ameer Taweel <ameertaweel2002@gmail.com>
2023-05-04 16:20:33 +0300

    n_min = 2

--- aggft/performance_check.py
+++ aggft/performance_check.py
@@ -33,7 +33,7 @@ CONSTANTS = {
     "N_VALUES": (50, 100, 200, 400),
     "PRIVACY_TYPES": ("mask", "encr"),
     "P_VALUES": (0, 0.01, 0.02, 0.05, 0.1, 0.2, 0.4),
-    "N_MIN_CONSTANTS": (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
+    # "N_MIN_CONSTANTS": (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9),
     "ROUND_LEN_F": lambda N: 0.1 * N,
     "K": int(2e20),
     "PRF_KEY_LEN": 32,
@@ -70,136 +70,138 @@ CONSTANTS["STARTING_PORT"] = (
     CONSTANTS["HTTP_STARTING_PORT"] if CONSTANTS["USE_HTTP"] else -1
 )
 
-for N_MIN_CONSTANT in CONSTANTS["N_MIN_CONSTANTS"]:
+# for N_MIN_CONSTANT in CONSTANTS["N_MIN_CONSTANTS"]:
+N_MIN_CONSTANT = "N/A"
 
-    for N in CONSTANTS["N_VALUES"]:
+for N in CONSTANTS["N_VALUES"]:
 
-        N_MIN = N_MIN_CONSTANT * N
-        ROUND_LEN = CONSTANTS["ROUND_LEN_F"](N)
+    # N_MIN = N_MIN_CONSTANT * N
+    N_MIN = 2
+    ROUND_LEN = CONSTANTS["ROUND_LEN_F"](N)
 
-        for PRIVACY_TYPE in CONSTANTS["PRIVACY_TYPES"]:
+    for PRIVACY_TYPE in CONSTANTS["PRIVACY_TYPES"]:
 
-            for P in CONSTANTS["P_VALUES"]:
+        for P in CONSTANTS["P_VALUES"]:
 
-                n_configs = 1 if P == 0 else CONSTANTS["RUNS_PER_CONFIG"]
+            n_configs = 1 if P == 0 else CONSTANTS["RUNS_PER_CONFIG"]
 
-                for config in link_utils.uniform_fail_configurations(N, P, n_configs):
+            for config in link_utils.uniform_fail_configurations(N, P, n_configs):
 
-                    args = {
-                        **CONSTANTS,
-                        "SM_COUNT": N,
-                        "N_MIN": N_MIN,
-                        "PRIVACY_TYPE": PRIVACY_TYPE,
-                        "P": P,
-                        "ROUND_LEN": ROUND_LEN,
-                    }
+                args = {
+                    **CONSTANTS,
+                    "SM_COUNT": N,
+                    "N_MIN": N_MIN,
+                    "PRIVACY_TYPE": PRIVACY_TYPE,
+                    "P": P,
+                    "ROUND_LEN": ROUND_LEN,
+                }
 
-                    # Generate Keys
-                    prf_keys = tuple(
-                        [random.randbytes(args["PRF_KEY_LEN"]) for _ in range(N)]
-                    )
-                    pk, sk = paillier.generate_paillier_keypair(
-                        n_length=CONSTANTS["PAILLIER_KEY_LEN"]
-                    )
+                # Generate Keys
+                prf_keys = tuple(
+                    [random.randbytes(args["PRF_KEY_LEN"]) for _ in range(N)]
+                )
+                pk, sk = paillier.generate_paillier_keypair(
+                    n_length=CONSTANTS["PAILLIER_KEY_LEN"]
+                )
 
-                    args = {**args, "prf_keys": prf_keys, "pk": pk, "sk": sk}
+                args = {**args, "prf_keys": prf_keys, "pk": pk, "sk": sk}
 
-                    registry = (
-                        None if CONSTANTS["USE_HTTP"] else shared_mem.make_registry(N)
-                    )
+                registry = (
+                    None if CONSTANTS["USE_HTTP"] else shared_mem.make_registry(N)
+                )
 
-                    # Helper functions
-                    base_dc_meta = (
-                        base_meta.base_dc_masking_meta(args)
-                        if PRIVACY_TYPE == "mask"
-                        else base_meta.base_dc_paillier_meta(args)
-                    )
-                    base_sm_meta = (
-                        base_meta.base_sm_masking_meta(args)
-                        if PRIVACY_TYPE == "mask"
-                        else base_meta.base_sm_paillier_meta(args)
-                    )
+                # Helper functions
+                base_dc_meta = (
+                    base_meta.base_dc_masking_meta(args)
+                    if PRIVACY_TYPE == "mask"
+                    else base_meta.base_dc_paillier_meta(args)
+                )
+                base_sm_meta = (
+                    base_meta.base_sm_masking_meta(args)
+                    if PRIVACY_TYPE == "mask"
+                    else base_meta.base_sm_paillier_meta(args)
+                )
 
-                    def make_net_mngr():
-                        if CONSTANTS["USE_HTTP"]:
-                            return network.HTTPNetworkManager()
-                        return network.SharedMemoryNetworkManager(registry)
+                def make_net_mngr():
+                    if CONSTANTS["USE_HTTP"]:
+                        return network.HTTPNetworkManager()
+                    return network.SharedMemoryNetworkManager(registry)
 
-                    dc_report, sm_reports = simulator.simulate(
-                        N,
-                        args["STARTUP_WAIT"],
-                        config,
-                        make_net_mngr,
-                        base_dc_meta,
-                        base_sm_meta,
-                        args["STARTING_PORT"],
-                    )
+                dc_report, sm_reports = simulator.simulate(
+                    N,
+                    args["STARTUP_WAIT"],
+                    config,
+                    make_net_mngr,
+                    base_dc_meta,
+                    base_sm_meta,
+                    args["STARTING_PORT"],
+                )
 
-                    terminated = int(dc_report.terminated)
-                    success = int(dc_report.success)
-                    dc_time = dc_report.t_end - dc_report.t_start
-                    dc_time_p_1 = dc_report.t_phase_1 - dc_report.t_start
-                    max_sm_time = max([i.t_end - i.t_start for i in sm_reports])
-                    min_sm_time = min([i.t_end - i.t_start for i in sm_reports])
-                    avg_sm_time = avg([i.t_end - i.t_start for i in sm_reports])
-                    std_sm_time = std([i.t_end - i.t_start for i in sm_reports])
-                    phase_1_cnt = dc_report.phase_1_count
-                    phase_2_cnt = dc_report.phase_2_count
-                    dc_net_snd_count = dc_report.net_snd
-                    dc_net_snd_size  = dc_report.net_snd_size
-                    dc_net_rcv_count = dc_report.net_rcv
-                    dc_net_rcv_size  = dc_report.net_rcv_size
-                    max_sm_snd_count = max([i.net_snd for i in sm_reports])
-                    max_sm_snd_size  = max([i.net_snd_size for i in sm_reports])
-                    max_sm_rcv_count = max([i.net_rcv for i in sm_reports])
-                    max_sm_rcv_size  = max([i.net_rcv_size for i in sm_reports])
-                    min_sm_snd_count = min([i.net_snd for i in sm_reports])
-                    min_sm_snd_size  = min([i.net_snd_size for i in sm_reports])
-                    min_sm_rcv_count = min([i.net_rcv for i in sm_reports])
-                    min_sm_rcv_size  = min([i.net_rcv_size for i in sm_reports])
-                    avg_sm_snd_count = avg([i.net_snd for i in sm_reports])
-                    avg_sm_snd_size  = avg([i.net_snd_size for i in sm_reports])
-                    avg_sm_rcv_count = avg([i.net_rcv for i in sm_reports])
-                    avg_sm_rcv_size  = avg([i.net_rcv_size for i in sm_reports])
-                    std_sm_snd_count = std([i.net_snd for i in sm_reports])
-                    std_sm_snd_size  = std([i.net_snd_size for i in sm_reports])
-                    std_sm_rcv_count = std([i.net_rcv for i in sm_reports])
-                    std_sm_rcv_size  = std([i.net_rcv_size for i in sm_reports])
-                    output = [
-                        N,
-                        N_MIN_CONSTANT,
-                        N_MIN,
-                        PRIVACY_TYPE,
-                        P,
-                        terminated,
-                        success,
-                        dc_time,
-                        dc_time_p_1,
-                        max_sm_time,
-                        min_sm_time,
-                        avg_sm_time,
-                        std_sm_time,
-                        phase_1_cnt,
-                        phase_2_cnt,
-                        dc_net_snd_count,
-                        dc_net_snd_size,
-                        dc_net_rcv_count,
-                        dc_net_rcv_size,
-                        max_sm_snd_count,
-                        max_sm_snd_size,
-                        max_sm_rcv_count,
-                        max_sm_rcv_size,
-                        min_sm_snd_count,
-                        min_sm_snd_size,
-                        min_sm_rcv_count,
-                        min_sm_rcv_size,
-                        avg_sm_snd_count,
-                        avg_sm_snd_size,
-                        avg_sm_rcv_count,
-                        avg_sm_rcv_size,
-                        std_sm_snd_count,
-                        std_sm_snd_size,
-                        std_sm_rcv_count,
-                        std_sm_rcv_size
-                    ]
-                    print(stringify(output))
+                terminated = int(dc_report.terminated)
+                success = int(dc_report.success)
+                dc_time = dc_report.t_end - dc_report.t_start
+                dc_time_p_1 = dc_report.t_phase_1 - dc_report.t_start
+                max_sm_time = max([i.t_end - i.t_start for i in sm_reports])
+                min_sm_time = min([i.t_end - i.t_start for i in sm_reports])
+                avg_sm_time = avg([i.t_end - i.t_start for i in sm_reports])
+                std_sm_time = std([i.t_end - i.t_start for i in sm_reports])
+                phase_1_cnt = dc_report.phase_1_count
+                phase_2_cnt = dc_report.phase_2_count
+                dc_net_snd_count = dc_report.net_snd
+                dc_net_snd_size  = dc_report.net_snd_size
+                dc_net_rcv_count = dc_report.net_rcv
+                dc_net_rcv_size  = dc_report.net_rcv_size
+                max_sm_snd_count = max([i.net_snd for i in sm_reports])
+                max_sm_snd_size  = max([i.net_snd_size for i in sm_reports])
+                max_sm_rcv_count = max([i.net_rcv for i in sm_reports])
+                max_sm_rcv_size  = max([i.net_rcv_size for i in sm_reports])
+                min_sm_snd_count = min([i.net_snd for i in sm_reports])
+                min_sm_snd_size  = min([i.net_snd_size for i in sm_reports])
+                min_sm_rcv_count = min([i.net_rcv for i in sm_reports])
+                min_sm_rcv_size  = min([i.net_rcv_size for i in sm_reports])
+                avg_sm_snd_count = avg([i.net_snd for i in sm_reports])
+                avg_sm_snd_size  = avg([i.net_snd_size for i in sm_reports])
+                avg_sm_rcv_count = avg([i.net_rcv for i in sm_reports])
+                avg_sm_rcv_size  = avg([i.net_rcv_size for i in sm_reports])
+                std_sm_snd_count = std([i.net_snd for i in sm_reports])
+                std_sm_snd_size  = std([i.net_snd_size for i in sm_reports])
+                std_sm_rcv_count = std([i.net_rcv for i in sm_reports])
+                std_sm_rcv_size  = std([i.net_rcv_size for i in sm_reports])
+                output = [
+                    N,
+                    N_MIN_CONSTANT,
+                    N_MIN,
+                    PRIVACY_TYPE,
+                    P,
+                    terminated,
+                    success,
+                    dc_time,
+                    dc_time_p_1,
+                    max_sm_time,
+                    min_sm_time,
+                    avg_sm_time,
+                    std_sm_time,
+                    phase_1_cnt,
+                    phase_2_cnt,
+                    dc_net_snd_count,
+                    dc_net_snd_size,
+                    dc_net_rcv_count,
+                    dc_net_rcv_size,
+                    max_sm_snd_count,
+                    max_sm_snd_size,
+                    max_sm_rcv_count,
+                    max_sm_rcv_size,
+                    min_sm_snd_count,
+                    min_sm_snd_size,
+                    min_sm_rcv_count,
+                    min_sm_rcv_size,
+                    avg_sm_snd_count,
+                    avg_sm_snd_size,
+                    avg_sm_rcv_count,
+                    avg_sm_rcv_size,
+                    std_sm_snd_count,
+                    std_sm_snd_size,
+                    std_sm_rcv_count,
+                    std_sm_rcv_size
+                ]
+                print(stringify(output))
